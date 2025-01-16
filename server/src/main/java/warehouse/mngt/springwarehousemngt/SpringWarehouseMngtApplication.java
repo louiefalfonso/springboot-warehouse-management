1389f0752313eec2d36fbaad0fb34cd1d26a8fd9
Louie Alfonso <lfriasalfonso@gmail.com>
2025-01-16 22:44:11 +0000

    Update Package Directory
    Update Package Directory

--- server/src/main/java/warehouse/mngt/springwarehousemngt/SpringWarehouseMngtApplication.java
+++ server/src/main/java/warehouse/mngt/springwarehousemngt/SpringWarehouseMngtApplication.java
@@ -1,10 +1,17 @@
-package com.example.spring_warehouse_mngt;
+package warehouse.mngt.springwarehousemngt;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringWarehouseMngtApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWarehouseMngtApplication.class, args);
