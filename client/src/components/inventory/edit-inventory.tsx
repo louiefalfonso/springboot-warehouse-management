fe91eabca599df6eddd6e60d4ac5b9e8ca555e41
Louie Alfonso <lfriasalfonso@gmail.com>
2025-06-05 16:34:45 +0100

    Added Services & UI Components for Inventory

---
+++ client/src/components/inventory/edit-inventory.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateInventory = () => {
+  return (
+    <div>UpdateInventory</div>
+  )
+}
+
+export default UpdateInventory
---
+++ client/src/components/inventory/list/list-inventory.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InventoryDetailsList = () => {
+  return (
+    <div>InventoryDetailsList</div>
+  )
+}
+
+export default InventoryDetailsList
