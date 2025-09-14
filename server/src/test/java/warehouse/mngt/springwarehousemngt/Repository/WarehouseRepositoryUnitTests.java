0d292b13a2b17c43ecf1373e49e4b882cc167c89
Louie Alfonso <lfriasalfonso@gmail.com>
2025-09-14 19:36:53 +0100

    Update Product & Warehouse Repository Unit Tests

--- server/src/test/java/warehouse/mngt/springwarehousemngt/Repository/WarehouseRepositoryUnitTests.java
+++ server/src/test/java/warehouse/mngt/springwarehousemngt/Repository/WarehouseRepositoryUnitTests.java
@@ -1,10 +1,9 @@
 package warehouse.mngt.springwarehousemngt.Repository;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
